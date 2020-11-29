bea53bc186788461656a312584cfc973f77b03a7
Ian Cossentino <icossentino@gmail.com>
2020-11-29 13:55:27 -0500

    commit tree 3fcc1a83ded5b7a7ae481d9f9163df9dcac90b22

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,25 @@
 
 
+
+class Course
+
+    @@all = []
+
+    attr_accessor :title, :schedule, :description
+
+    def self.all
+        @@all
+    end
+
+    def initialize
+        self.class.all << self
+    end
+
+    def self.reset_all
+        self.all.clear
+    end
+
+
+
+
+end
